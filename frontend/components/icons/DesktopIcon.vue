e25d3b840dcba8d3d764d54e76a5934c9326449f
Anuj Datar <anuj.datar@gmail.com>
2022-06-07 00:02:31 +0530

    Added sun, moon and desktop icon components for theming

---
+++ frontend/components/icons/DesktopIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6Zm6 14h8"/>
+  </svg>
+</template>
