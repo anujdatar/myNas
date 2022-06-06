16263e4c53a80c99d80d7528cbfd81132043ac60
Anuj Datar <anuj.datar@gmail.com>
2022-06-07 00:05:02 +0530

    updated main app.vue

---
+++ frontend/app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+/* global useHead */
+useHead({
+  titleTemplate: 'myNAS - %s',
+  meta: [
+    { name: 'color-scheme', content: 'dark light' }
+  ]
+})
+</script>
+
+<template>
+  <nuxt-layout>
+    <nuxt-page />
+  </nuxt-layout>
+</template>
