0a6183cd042995ca8eb89066697eb1d0cd077c9d
Anuj Datar <anuj.datar@gmail.com>
2022-06-07 08:21:28 +0530

    Added a dummy sidebar component for now

---
+++ frontend/src/components/MyNasSidebar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="sidebar">
+    <ul>
+      <li>Users & Groups</li>
+      <li>Drives</li>
+      <li>Services</li>
+      <li>System</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.sidebar {
+  width: var(--sidebar-width);
+  background-color: var(--surface);
+  height: 100vh;
+  /* border-right: 1px solid var(--foreground); */
+}
+</style>
